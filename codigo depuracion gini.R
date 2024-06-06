33b5bda3457cf1a646759cc3607c9525f93f767b
CamilaMatellicani <camilamatellicani@gmail.com>
2024-06-06 12:19:00 -0300

    prueba merge

--- codigo depuracion gini.R
+++ codigo depuracion gini.R
@@ -9,6 +9,12 @@ names(gini_sucia)[names(gini_sucia) == "Country Name"] <- "Pais"
 names(gini_sucia)[names(gini_sucia) == "Country Code"] <- "Codigo"
 
 
+sum(esperanza_de_vida$`Location type`!= "Country")
+codigo_paises <- esperanza_de_vida$SpatialDimValueCode
+
 #grafico de series por pais para demostrar por que tomar el valor de 2019 o mas reciente
 #hasta 2010
 
+#filtro_paisesifelse(gini_sucia$Codigo codigo_paises,1,0)
+
+datos_mersheados <- merge(gini_sucia, esperanza_de_vida, by.x="Codigo", by.y = "SpatialDimValueCode")
